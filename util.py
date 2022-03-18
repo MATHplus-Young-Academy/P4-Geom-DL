205b9cc4cec5953db02f26d862bdbc957e483303
Felix Herter <herterfe@gmail.com>
2022-03-18 13:08:18 +0000

    add function to generate test dataset

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ import torch
 import pyvista as pv
 from pathlib import Path
 import csv
+import math
 import re
 import numpy as np
 from collections import defaultdict
@@ -12,9 +13,7 @@ from collections import defaultdict
 plys_to_poi_filename = { 'Bohunician': 'Bohunician', 'PunchedBlade': 'PunchedBlade', 'Shea_Levallois': 'SheaLevallois'}
 base_path = Path('/mnt/materials/SIRF/MathPlusBerlin/DATA/LihicFlakesPOI/')
 
-def label_to_data_object(dataset, label):
-    filename = label_to_filename_map(dataset)[label][0]
-    mesh = pv.read(base_path / f'3DModels_plys/{dataset}_plys/{filename}')
+def mesh_to_data_object(mesh):
     pos = torch.tensor(mesh.points, dtype=torch.float32)
     face = torch.tensor(mesh.faces.reshape(-1,4)[:,1:].T, dtype=torch.long)
     norm = torch.tensor(mesh.point_normals, dtype=torch.float32)
@@ -24,6 +23,11 @@ def label_to_data_object(dataset, label):
     
     return transform(graph)
 
+def label_to_data_object(dataset, label):
+    filename = label_to_filename_map(dataset)[label][0]
+    mesh = pv.read(base_path / f'3DModels_plys/{dataset}_plys/{filename}')
+    return mesh_to_data_object(mesh)
+    
 def get_labels(dataset):
     poi = plys_to_poi_filename[dataset]
     POI_file = base_path / f'POIs_csv/{poi}_POIs_Point.csv'
@@ -69,3 +73,66 @@ def get_dataloader(dataset, **kwargs):
         d.y = pois[l]
         
     return DataLoader(list(datas.values()), **kwargs)
+
+# Dummy Data generation 
+
+def fibonacci_sphere(samples=1000):
+    # https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
+    points = []
+    phi = math.pi * (3. - math.sqrt(5.))  # golden angle in radians
+
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = math.sqrt(1 - y * y)  # radius at y
+
+        theta = phi * i  # golden angle increment
+
+        x = math.cos(theta) * radius
+        z = math.sin(theta) * radius
+
+        points.append((x, y, z))
+
+    return np.asarray(points)
+
+def generate_cone_meshes(num_cones=100):
+    directions = fibonacci_sphere(num_cones)
+    center = np.array((0,0,0))
+    radius = 0.2
+    height = 1
+
+    cones = [pv.Cone(center=center, direction=d, height=height, radius=radius) for d in directions]
+    apexes = [center + d * height / 2 for d in directions]
+
+    return [cones, apexes]
+            
+def triangulate_face(face):
+    assert(face[0] >= 3)
+    if face[0] == 3:
+        return np.array(face)
+    result = []
+    a = face[1]
+    for b, c in zip (face[2:], face[3:]):
+        result += [3, a, b, c]
+    return np.asarray(result)
+
+def triangulate_faces(faces):
+    triangulated = []
+    i = 0
+    while i < len(faces):
+        face = faces[i:i+faces[i]+1]
+        triangulated.append(triangulate_face(face))
+        i += faces[i] + 1
+    return np.concatenate(triangulated)
+
+def generate_cone_data_objects(num_cones=100):
+    cones, apexes = generate_cone_meshes(num_cones)
+    
+    for cone in cones:
+        cone.faces = triangulate_faces(cone.faces)
+    
+    result = []
+    for cone, apex in zip(cones, apexes):
+        mesh = mesh_to_data_object(cone)
+        mesh.y = apex
+        result.append(mesh)
+    return result
